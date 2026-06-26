4c8cb02678fb94c5c82bdc9042ec82589fa337cd
Solara-Tech <chaeltayer@gmail.com>
2026-06-26 15:24:28 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -6,5 +6,5 @@
 window.RMA_CONFIG = {
   SUPABASE_URL:      "https://ahfillwuukqznfmicria.supabase.co",
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFoZmlsbHd1dWtxem5mbWljcmlhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI0NDk1MjksImV4cCI6MjA5ODAyNTUyOX0.bmeF3uelcfRnUCkhgvqQK7Arj5S-mSZ4JtXeTnHFpeg",
-  TEAM_EMAIL:        "rmainnotivetech@gmail.com"
+  TEAM_EMAIL:        "rmainnovativetech@gmail.com"
 };
